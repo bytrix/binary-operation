839a15c9156a77cf55e407de80e6ae5619e473ab
bytrix <wengzhijie@gmail.com>
2016-09-03 16:54:29 +0800

    first upload

---
+++ Bin.java
@@ -0,0 +1,48 @@
+package com.system;
+import java.io.*;
+
+public class Bin {
+	// public static void main(String[] args) {
+		
+		/*
+		File out = new File("out");
+		try {
+			out.createNewFile();
+			FileWriter fw = new FileWriter(out);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(int i=-129; i<128; i++) {
+				bw.write(String.format("%04d", i) + ": " + toBin(i) + "\n");
+			}
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		*/
+	// }
+
+	public static String parseBin(int x) {
+		int[] num = new int[]{0,0,0,0,0,0,0,0};
+		StringBuilder sb = new StringBuilder();
+		int index = 7;
+		if(x < 0) {
+			num[0] = 1;
+			x = -x;
+		}
+		while(true) {
+			num[index] = x%2;
+			if(x/2 !=0 ) {
+				x = x/2;
+			} else {
+				break;	
+			}
+			if(index > 0) {
+				index--;
+			}
+		}
+
+		for(int n : num) {
+			sb.append(n);
+		}
+		return sb.toString();
+	}
+}
---
+++ Number.java
@@ -0,0 +1,118 @@
+package com.system;
+import com.system.Bin;
+
+class Number {
+
+	private int trueValue;
+	private String mSignAndMagnitude;
+	private String mOnesComplement;
+	private String mTwosComplement;
+
+	Number(int x) {
+		// setSignAndMagnitude(Bin.parseBin(x));
+		mSignAndMagnitude = new String();
+		mOnesComplement = new String();
+		mTwosComplement = new String();
+
+		trueValue = x;
+
+		mSignAndMagnitude = Bin.parseBin(x);
+
+		setOnesComplement();
+
+		setTwosComplement();
+	}
+
+	public int getTrueValue() {
+		return trueValue;
+	}
+
+	public String getSignAndMagnitude() {
+		return mSignAndMagnitude;
+	}
+
+	public String getOnesComplement() {
+		return mOnesComplement;
+	}
+
+	public String getTwosComplement() {
+		return mTwosComplement;
+	}
+
+	// public String setSignAndMagnitude(String x) {
+	// 	mSignAndMagnitude = x;
+	// }
+
+	public void setOnesComplement() {
+		// mOnesComplement = x;
+		// int signBit;
+		// System.out.println("aa");
+		if (trueValue < 0) {
+			int[] arrSignAndMagnitude = new int[8];
+			for (int i=0; i<8; i++) {
+				arrSignAndMagnitude[i] = mSignAndMagnitude.charAt(i) - 48;
+				// System.out.println(mSignAndMagnitude.charAt(i) + " " + arrSignAndMagnitude[i]);
+			}
+			// signBit = arrSignAndMagnitude[0];
+			for (int i=1; i<8; i++) {
+				arrSignAndMagnitude[i] = arrSignAndMagnitude[i]==1 ? 0 : 1;
+				// if (arrSignAndMagnitude[i] == 1) {
+				// 	arrSignAndMagnitude[i] = 0;
+				// } else {
+				// 	arrSignAndMagnitude[i] = 1;
+				// }
+				// System.out.println(arrSignAndMagnitude[i]);
+			}
+			for (int i=0; i<8; i++) {
+				mOnesComplement += arrSignAndMagnitude[i];
+			}
+			// System.out.println(arrSignAndMagnitude);
+
+			// for(int i=0; i<8; i++) {
+				// System.out.println(arrSignAndMagnitude[i]);
+				// mOnesComplement += "a";
+			// }
+		} else {
+			mOnesComplement = mSignAndMagnitude;
+		}
+		// return null;
+
+	}
+
+	public void setTwosComplement() {
+		if (trueValue < 0) {
+			int carryBit = 1;
+			int signBit;
+			int[] arrTwosComplement = new int[8];
+			int[] tempBits = new int[8];
+			// System.out.println("length: " + getOnesComplement().length());
+			for(int i=0; i<8; i++) {
+				arrTwosComplement[i] = mOnesComplement.charAt(i) - 48;
+			}
+			signBit = arrTwosComplement[0];
+			for(int i=7; i>0; i--) {
+				// System.out.println(i);
+				// switch
+				if (arrTwosComplement[i]+carryBit == 0) {
+					tempBits[i] = 0;
+					carryBit = 0;
+				} else if(arrTwosComplement[i]+carryBit == 1) {
+					tempBits[i] = 1;
+					carryBit = 0;
+				} else if(arrTwosComplement[i]+carryBit == 2) {
+					tempBits[i] = 0;
+					carryBit = 1;
+				}
+			}
+
+			mTwosComplement = signBit+"";
+			for(int i=1; i<8; i++) {
+				mTwosComplement += tempBits[i];
+			}
+		} else {
+			mTwosComplement = mSignAndMagnitude;
+		}
+
+		// mTwoComplement = x;
+	}
+}
