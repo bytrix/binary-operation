191174b6631b012904adcae91c392ee5fbebdec9
bytrix <wengzhijie@gmail.com>
2016-09-04 12:23:26 +0800

    Add IntegerNumber.class in jar file

--- test/Test.java
+++ test/Test.java
@@ -1,4 +1,5 @@
 import com.system.Bin;
+import com.system.IntegerNumber;
 import com.system.FloatNumber;
 import com.system.Decimal;
 import java.io.*;
@@ -6,10 +7,6 @@ import java.text.DecimalFormat;
 
 class Test {
 	public static void main(String[] args) {
-		// System.out.println(Decimal.parseDecimal("10000011"));
-		// System.out.println(Decimal.parseDecimalF("1.0000001"));
-		// FileOutputStream fos = new FileOutputStream(file);
-
 
 		try {
 
@@ -25,17 +22,19 @@ class Test {
 
 				double trueValue = Decimal.parseDecimalF(line);
 				FloatNumber n = new FloatNumber(line, trueValue);
-				// DecimalFormat format = new DecimalFormat("0.0000000");
+
 				System.out.println(String.format("% 10f", trueValue) + "  " + line + " " + n.getOnesComplement() + " " + n.getTwosComplement());
-				// System.out.println(String.format("% 9d", format.format(trueValue)));
-				// System.out.println(String.format("%09d", 5));
-				// System.out.println(String.format("%020f", trueValue) + " " + line);
+
 			}
 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 
-		// System.out.println("1234");
+		// for(int i=-128; i<=127; i++) {
+		// 	IntegerNumber n = new IntegerNumber(i);
+		// 	System.out.println(String.format("% 5d", i) + " " + n.getSignAndMagnitude() + " " + n.getOnesComplement() + " " + n.getTwosComplement());
+		// }
+
 	}
 }
